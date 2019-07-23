2b99adfdd9e5339ec2e76f463bb02d4d1388949a
Stdio <kwork-stdio@mail.ru>
2019-07-23 21:38:18 +0300

    0.0.3
    mapView added

--- app/src/main/java/com/sample/currencyconverter/MainActivity.java
+++ app/src/main/java/com/sample/currencyconverter/MainActivity.java
@@ -1,18 +1,32 @@
 package com.sample.currencyconverter;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Typeface;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Lifecycle;
 import androidx.viewpager.widget.ViewPager;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.sample.currencyconverter.fragments.FragmentMap;
 
 public class MainActivity extends AppCompatActivity {
 
     CustomPageAdapter mCustomPageAdapter;
     ViewPager mViewPager;
+    public static double latitude = -34;
+    public static double longitude = 151;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public static LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +35,85 @@ public class MainActivity extends AppCompatActivity {
 
         mCustomPageAdapter = new CustomPageAdapter(getSupportFragmentManager(), this);
 
-        //getPosisi.getItem();
-
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mCustomPageAdapter);
-
         mViewPager.getCurrentItem();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.INITIALIZED)
+                && !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            new GpsUtils(this).turnGPSOn();
+        }
+        callPositionRequest();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.INITIALIZED)
+                && !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            new GpsUtils(MainActivity.this).turnGPSOn();
+        }
+    }
+
+    private void callPositionRequest() {
+        LocationRequest req = new LocationRequest();
+        if (checkLocationPermission() && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // Permission has already been granted
+            final FusedLocationProviderClient locationClient = LocationServices.
+                    getFusedLocationProviderClient(this);
+
+            locationClient.requestLocationUpdates(req, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+
+                    latitude = locationResult.getLastLocation().getLatitude();
+                    longitude = locationResult.getLastLocation().getLongitude();
+                }
+            }, null);
+        }
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            callPositionRequest();
+            return false;
+        } else {
+            FragmentMap.permissionsIsGranted = true;
+            return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            callPositionRequest();
+        }
     }
 
 }
--- app/src/main/java/com/sample/currencyconverter/fragments/FragmentMap.java
+++ app/src/main/java/com/sample/currencyconverter/fragments/FragmentMap.java
@@ -1,25 +1,42 @@
 package com.sample.currencyconverter.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.sample.currencyconverter.MainActivity;
 import com.sample.currencyconverter.R;
 
+import static com.sample.currencyconverter.MainActivity.latitude;
+import static com.sample.currencyconverter.MainActivity.longitude;
 
-/**
- * Created by ovix on 10/10/17.
- */
 
-public class FragmentMap extends Fragment {
+public class FragmentMap extends Fragment implements OnMapReadyCallback {
 
     Boolean isStarted = false;
     Boolean isVisible = false;
+    private GoogleMap mMap;
+    public static boolean permissionsIsGranted = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle SaveInstanceState){
@@ -43,7 +60,65 @@ public class FragmentMap extends Fragment {
         super.setUserVisibleHint(isVisibleToUser);
         isVisible = isVisibleToUser;
         if (isStarted && isVisible) {
-
+            if (permissionsIsGranted && MainActivity.locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                initMap();
+                getCurrentLocation();
+            }
         }
+    }
+
+    private void getCurrentLocation() {
+        final FusedLocationProviderClient locationClient = LocationServices.
+                getFusedLocationProviderClient(getContext());
+        try {
+            Task<Location> location = locationClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+
+                    if (task.getResult() == null) {
+                        getCurrentLocation();
+                    }
+                    else {
+                        latitude = task.getResult().getLatitude();
+                        longitude = task.getResult().getLongitude();
+                    }
+                    initMap();
+                }
+            });
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void initMap() {
+        String MAP_FRAGMENT = "map_fragment";
+
+        FragmentManager fragmentManager = getChildFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fragmentManager
+                .findFragmentByTag(MAP_FRAGMENT); // Check if map already exists
+
+        if(mapFragment == null){
+            // Create new Map instance if it doesn't exist
+            mapFragment = SupportMapFragment.newInstance();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.map, mapFragment, MAP_FRAGMENT)
+                    .commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng currentPosition = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(currentPosition).title("Ваше местоположение"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(5.0f));
     }
 }
